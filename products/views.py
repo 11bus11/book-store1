8c0c404cdd708bce3471423a33ef36ddb1478216
Erik Vodopivec Forsman <02linfor01@gmail.com>
2022-12-26 00:45:58 +0000

    created product detail page

--- products/views.py
+++ products/views.py
@@ -21,3 +21,16 @@ def all_products(request):
         }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """view that shows product details for chosen product."""
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+            'product': product,
+        }
+
+    return render(request, 'products/product_detail.html', context)
+
