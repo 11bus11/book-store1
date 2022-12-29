eb1f73245a1890ab5b4d7c64a53a60ce26f22300
Erik Vodopivec Forsman <02linfor01@gmail.com>
2022-12-29 16:44:23 +0000

    added bag function, having some troube

--- bag/urls.py
+++ bag/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.bag, name='bag'), 
+    path('', views.bag, name='bag'),
+    path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
 ]
--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,11 @@
 
-from django.shortcuts import render
+from django.shortcuts import (
+    render, redirect, reverse, HttpResponse, get_object_or_404
+)
+
+from django.contrib import messages
+
+from products.models import Product
 
 # Create your views here.
 
@@ -8,3 +14,21 @@ def bag(request):
     """view that shows the shopping cart"""
     return render(request, 'bag/bag.html')
 
+
+def add_to_bag(request, item_id):
+    """Add items to shopping bag"""
+
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+
+    request.session['bag'] = bag
+    print("test")
+
+    return redirect(redirect_url)
