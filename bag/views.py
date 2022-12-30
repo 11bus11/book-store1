76e901448ed5d2d958106ad342282b1758c6ba32
Erik Vodopivec Forsman <02linfor01@gmail.com>
2022-12-30 23:34:20 +0000

    Started remove from bag function

--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,7 @@
 
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import (
+    render, redirect, reverse, HttpResponse, get_object_or_404
+)
 
 from django.contrib import messages
 
@@ -9,31 +11,16 @@ from products.models import Product
 
 
 def bag(request):
-    """view that shows the shopping cart"""
+    """ A view that renders the bag contents page """
+
     return render(request, 'bag/bag.html')
-
-
-def adjust_bag(request, item_id):
-    """Add items to shopping bag"""
-
-    product = get_object_or_404(Product, pk=item_id)
-    quantity = int(request.POST.get('quantity'))
-    redirect_url = request.POST.get('redirect_url')
-    bag = request.session.get('bag', {})
-
-    if quantity > 0:
-        bag[item_id] = quantity
-    else:
-        bag.pop[item_id] = quantity
-
-    request.session['bag'] = bag
-    return redirect(redirect_url)
 
 
 def add_to_bag(request, item_id):
     """Add items to shopping bag"""
 
     product = get_object_or_404(Product, pk=item_id)
+    redirect_url = request.POST.get('redirect_url')
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
 
@@ -43,4 +30,20 @@ def add_to_bag(request, item_id):
         bag[item_id] = quantity
 
     request.session['bag'] = bag
-    return redirect(reverse('bag'))
+    return redirect(redirect_url)
+
+def remove_from_bag(request, item_id):
+    """Add items to shopping bag"""
+    try:
+        product = get_object_or_404(Product, pk=item_id)
+        bag = request.session.get('bag', {})
+        bag.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your bag')
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
+
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,7 +1,12 @@
-/*!
-* Start Bootstrap - Shop Homepage v5.0.5 (https://startbootstrap.com/template/shop-homepage)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+// Remove item on click
+$('.remove-item').click(function(e) {
+    var csrfToken = "{{ csrf_token }}";
+    var itemId = $(this).attr('id').split('remove_')[1];
+    var url = '/bag/remove/$(itemId)';
+    var data = {"csrfmiddlewaretoken": csrfToken};
+
+    $.post(url, data)
+     .done(function() {
+        location.reload();
+     });
+})
