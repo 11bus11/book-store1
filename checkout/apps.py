10f1525d0a8971c0ba00cac0a63e0e0777341ea2
Erik Vodopivec Forsman <108095837+11bus11@users.noreply.github.com>
2022-12-31 21:23:06 +0100

    registered models checkout, added signals

--- checkout/apps.py
+++ checkout/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class CheckoutConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'checkout'
+
+    def ready(delf):
+        import checkout.signals
