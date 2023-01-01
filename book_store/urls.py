e295b2e6880bbedbef27a9c4eb8e4629a34f3220
Erik Vodopivec Forsman <02linfor01@gmail.com>
2023-01-01 17:39:03 +0000

    started template and created view + url for checkout

--- book_store/urls.py
+++ book_store/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('articles/', include('articles.urls')),
     path('bag/', include('bag.urls')),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
