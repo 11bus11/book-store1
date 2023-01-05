769a05d5ed36422a32008bf7af77a84763193850
Erik Vodopivec Forsman <02linfor01@gmail.com>
2023-01-05 18:29:42 +0000

    started webhook handler

---
+++ checkout/webhook_handler.py
@@ -0,0 +1,27 @@
+from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
+
+from .models import Order, OrderLineItem
+from products.models import Product
+#from profiles.models import UserProfile
+
+import json
+import time
+
+class StripeWH_Handler:
+    """Handle Stripe webhooks"""
+
+    def __init__(self, request):
+        self.request = request
+
+    def handle_event(self, event):
+        """
+        Handle a generic/unknown/unexpected webhook event
+        """
+        return HttpResponse(
+            content=f'Unhandled webhook received: {event["type"]}',
+            status=200)
+
+   
