d755d97be96e1b76aa1be0df2d8cbc1b94b7d2d5
Erik Vodopivec Forsman <02linfor01@gmail.com>
2023-01-05 16:52:12 +0000

    now possible to place orders and pay

--- checkout/urls.py
+++ checkout/urls.py
@@ -4,6 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
-    path('checkout-success/', views.checkout_success, name='checkout-success'),
-    path('cache_checkout_data/', views.cache_checkout_data, name='cache_checkout_data'),
+    path('checkout_success/<order_number>',
+         views.checkout_success,
+         name='checkout_success'),
+    path('cache_checkout_data/',
+         views.cache_checkout_data,
+         name='cache_checkout_data'),
 ]
