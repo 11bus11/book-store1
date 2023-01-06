ea897345cff2ebefa6ced5ba210db4266c12936f
Erik Vodopivec Forsman <02linfor01@gmail.com>
2023-01-06 01:05:27 +0000

    styled account pages (login and more)

--- profiles/views.py
+++ profiles/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import UserProfile
-from .forms import UserProfileForm
+#from .forms import UserProfileForm
 
 from checkout.models import Order
 
@@ -12,20 +12,20 @@ def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
 
-    if request.method == 'POST':
-        form = UserProfileForm(request.POST, instance=profile)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Profile updated successfully')
-        else:
-            messages.error(request, 'Update failed. Please ensure the form is valid.')
-    else:
-        form = UserProfileForm(instance=profile)
+    #if request.method == 'POST':
+    #    form = UserProfileForm(request.POST, instance=profile)
+    #    if form.is_valid():
+    #        form.save()
+    #        messages.success(request, 'Profile updated successfully')
+    #    else:
+    #        messages.error(request, 'Update failed. Please ensure the form is valid.')
+    #else:
+    #    form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
     context = {
-        'form': form,
+        #'form': form,
         'orders': orders,
         'on_profile_page': True
     }
