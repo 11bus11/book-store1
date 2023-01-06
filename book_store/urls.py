545a8169f3abd28452730b5db419849b40ee2a4b
Erik Vodopivec Forsman <02linfor01@gmail.com>
2023-01-06 00:06:00 +0000

    added profiles view and url

--- book_store/urls.py
+++ book_store/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('articles/', include('articles.urls')),
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
