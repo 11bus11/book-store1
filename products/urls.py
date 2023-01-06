0a2c5475f17e9a41ab5397a2eafd245bc865918a
Erik Vodopivec Forsman <02linfor01@gmail.com>
2023-01-06 18:14:12 +0000

    added delete product function to frontend.

--- products/urls.py
+++ products/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('<int:product_id>', views.product_detail, name='product_detail'),
     path('add/', views.add_product, name='add_products'),
     path('edit/<int:product_id>/', views.edit_product, name='edit_products'),
+    path('delete/<int:product_id>/', views.delete_product, name='delete_products'),
 ]
