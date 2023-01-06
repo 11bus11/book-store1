58abe0d2912201a17822de9829457d6f5bc14123
Erik Vodopivec Forsman <02linfor01@gmail.com>
2023-01-06 23:13:41 +0000

    installed brotli + whitenoise

--- book_store/settings.py
+++ book_store/settings.py
@@ -166,7 +166,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
