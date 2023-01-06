582cbcecb87959fb3f7ec3ea12a5eb3f4da45f36
Erik Vodopivec Forsman <02linfor01@gmail.com>
2023-01-06 01:56:52 +0000

    finished profile form on profile page

---
+++ profiles/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from .models import UserProfile
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        exclude = ('user',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'name': 'Name',
+            'email': 'Email Address',
+        }
+
+        self.fields['name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
--- profiles/models.py
+++ profiles/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+from django_countries.fields import CountryField
 
 class UserProfile(models.Model):
     """
@@ -10,7 +11,9 @@ class UserProfile(models.Model):
     delivery information and order history
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20, null=True, blank=True)
     email = models.CharField(max_length=20, null=True, blank=True)
+    country = CountryField(blank_label='Country', null=True, blank=True)
 
     def __str__(self):
         return self.user.username
